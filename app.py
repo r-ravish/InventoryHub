dcdc4d7f763d5bbedc6d74809a425192147eaea0
ravish <www.ravishraheja@gmail.com>
2025-02-17 13:49:44 +0530

    updated

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
